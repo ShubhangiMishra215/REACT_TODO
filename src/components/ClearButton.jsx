174b2d2c8e0d6d5b13f9d480bb14c3358fd7f61f
Shubhangi Mishra <shubhangimishra215@gmail.com>
2026-05-17 09:28:43 +0530

    fix: clear button border and spacing

--- src/components/ClearButton.jsx
+++ src/components/ClearButton.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
 const ClearButton = ({ onClick, completedTodos}) => {
-  if(completedTodos===0) return null;
+  
   return (
-    <>
-     <button
-     onClick={onClick}
-     className='mt-4 w-full py-3 backdrop-blur-2xl bg-white/5 
-     hover:bg-white/10 text-white/70 hover:text-white transition-all duration-300
-     rounded-lg font-semibold border-white/10 hover:scale-95 active:scale-95 text-sm'>
-        clear{completedTodos} completed tasks
-      </button> 
-    </>
+    <button
+      onClick={onClick}
+      className='mt-4 w-full py-3 backdrop-blur-2xl bg-white/5 
+      hover:bg-white/10 text-white/70 hover:text-white transition-all duration-300
+      rounded-lg font-semibold border border-white/10 hover:scale-95 active:scale-95 text-sm'>
+        Clear {completedTodos} completed tasks
+    </button> 
   )
 }
 
